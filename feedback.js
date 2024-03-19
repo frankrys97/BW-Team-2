721d69446a61411f3e4563e7a8949e78ed81bfe1
Lorenzo-Foschetti <lorenzo.foschetti00@gmail.com>
2024-03-19 10:34:23 +0100

    adds js

--- feedback.js
+++ feedback.js
@@ -12,6 +12,17 @@ function resetStars() {
     star.classList.remove("blueStars");
   });
 }
+function keepStars(num) {
+  for (let i = 0; i < num; i++) {
+    stars[i].classList.add("keepStars");
+  }
+}
+function resetKeepStars() {
+  stars.forEach((star) => {
+    star.classList.remove("keepStars");
+  });
+}
+
 //metodo forEach per aggiungere l'evento mouseover con relativa funzione di aggiunta classe
 stars.forEach((star, index) => {
   star.addEventListener("mouseover", () => {
@@ -21,4 +32,25 @@ stars.forEach((star, index) => {
   star.addEventListener("mouseout", () => {
     resetStars();
   });
+  star.addEventListener("click", () => {
+    resetKeepStars();
+    keepStars(index + 1);
+  });
+});
+const feedbackContainer = document.getElementById("response");
+const feedbackButton = document.getElementById("feedbackButton");
+feedbackButton.addEventListener("click", () => {
+  while (feedbackContainer.firstChild) {
+    feedbackContainer.removeChild(feedbackContainer.firstChild);
+  }
+  const numStars = document.querySelectorAll(".keepStars").length;
+  const feedbackMessage = document.createElement("p");
+  if (numStars >= 1 && numStars <= 6) {
+    feedbackMessage.innerText = "We are sorry, we'll do better the next time!";
+  } else if (numStars === 0) {
+    feedbackMessage.innerText = "No stars selected";
+  } else {
+    feedbackMessage.innerText = "Thanks for your feedback!";
+  }
+  feedbackContainer.appendChild(feedbackMessage);
 });
